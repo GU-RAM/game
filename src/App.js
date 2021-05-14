dfee1d8429ca68f3b193994b20a19b194d504854
guro <guramidgebuadze@gmail.com>
2021-05-14 12:46:02 +0400

    improve code

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-// import tamo from 'tamo.jpg';
+import questions from './components/questions';
 
 function App() {
-  const questions = [
-    {
-      questionText: 'საყვარელი ფერი',
-      answerOptions: [
-        { answerText: 'ცისფერი', isCorrect: false },
-        { answerText: 'მწვანე', isCorrect: false },
-        { answerText: 'ფორთოხლისფერი', isCorrect: true },
-        { answerText: 'ყვითელი', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'ფავორიტი წიგნი',
-      answerOptions: [
-        { answerText: 'სამი მეგობარი', isCorrect: false },
-        { answerText: 'სამი გოჭი', isCorrect: false },
-        { answerText: 'ძმები კარამაზოვები', isCorrect: true },
-        { answerText: 'დათა თუთაშხია', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'მთავარი ფასეულობა',
-      answerOptions: [
-        { answerText: 'ერთგულება', isCorrect: false },
-        { answerText: 'სიყვარული', isCorrect: true },
-        { answerText: 'კაცთმოყვარეობა', isCorrect: false },
-        { answerText: 'სიკეთე', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'მომხიბვლელობის მთავარი ელემენტი ქალში',
-      answerOptions: [
-        { answerText: 'ღიმილი', isCorrect: false },
-        { answerText: 'ლოყები', isCorrect: false },
-        { answerText: 'ინტელექტი', isCorrect: true },
-        { answerText: 'ძუძუები', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'სიმშვიდის წყარო',
-      answerOptions: [
-        { answerText: 'თამო', isCorrect: false },
-        { answerText: 'თამარი', isCorrect: false },
-        { answerText: 'თამჩაჩო', isCorrect: true },
-        { answerText: 'თამჩექსა', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'საყვარელი სპორტი',
-      answerOptions: [
-        { answerText: 'ფეხბურთი', isCorrect: false },
-        { answerText: 'კალათბურთი', isCorrect: true },
-        { answerText: 'ენეფელი', isCorrect: false },
-        { answerText: 'ჩოგბურთი', isCorrect: false },
-      ],
-    },
-  ];
-
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [points, setPoints] = useState(0);
   const [showScore, setShowScore] = useState(true);
---
+++ src/components/questions.js
@@ -0,0 +1,58 @@
+const questions = [
+  {
+    questionText: 'საყვარელი ფერი',
+    answerOptions: [
+      { answerText: 'ცისფერი', isCorrect: false },
+      { answerText: 'მწვანე', isCorrect: false },
+      { answerText: 'ფორთოხლისფერი', isCorrect: true },
+      { answerText: 'ყვითელი', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'ფავორიტი წიგნი',
+    answerOptions: [
+      { answerText: 'სამი მეგობარი', isCorrect: false },
+      { answerText: 'სამი გოჭი', isCorrect: false },
+      { answerText: 'ძმები კარამაზოვები', isCorrect: true },
+      { answerText: 'დათა თუთაშხია', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'მთავარი ფასეულობა',
+    answerOptions: [
+      { answerText: 'ერთგულება', isCorrect: false },
+      { answerText: 'სიყვარული', isCorrect: true },
+      { answerText: 'კაცთმოყვარეობა', isCorrect: false },
+      { answerText: 'სიკეთე', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'მომხიბვლელობის მთავარი ელემენტი ქალში',
+    answerOptions: [
+      { answerText: 'ღიმილი', isCorrect: false },
+      { answerText: 'ლოყები', isCorrect: false },
+      { answerText: 'ინტელექტი', isCorrect: true },
+      { answerText: 'ძუძუები', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'სიმშვიდის წყარო',
+    answerOptions: [
+      { answerText: 'თამო', isCorrect: false },
+      { answerText: 'თამარი', isCorrect: false },
+      { answerText: 'თამჩაჩო', isCorrect: true },
+      { answerText: 'თამჩექსა', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'საყვარელი სპორტი',
+    answerOptions: [
+      { answerText: 'ფეხბურთი', isCorrect: false },
+      { answerText: 'კალათბურთი', isCorrect: true },
+      { answerText: 'ენეფელი', isCorrect: false },
+      { answerText: 'ჩოგბურთი', isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
